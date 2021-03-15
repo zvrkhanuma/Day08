edf505f3ddfa4613dc5ec222ebcc545b7a90428a
zvrkhanuma <almazalovic@gmail.com>
2021-03-15 22:01:16 +0100

    Added second function

---
+++ ex02/src/index.js
@@ -0,0 +1,10 @@
+// Write code below this line
+function functionPrintInfo(name, age){
+console.log(name, age);
+
+}
+// Write code above this line
+module.exports = functionPrintInfo;
+
+functionPrintInfo("John", 23);
+functionPrintInfo("Michael", 43);
