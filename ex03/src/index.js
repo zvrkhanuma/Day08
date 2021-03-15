ef0aafc0abb42c5854fb4e5dff60ec71968dcbee
zvrkhanuma <almazalovic@gmail.com>
2021-03-15 23:01:44 +0100

    Added third function

---
+++ ex03/src/index.js
@@ -0,0 +1,23 @@
+// Write code below this line
+function function1(){
+    notGlobalVariable = 5;
+}
+var myGlobalVariable = 10;
+// Write code above this line
+function function2(){
+    var result = "";
+    if (typeof myGlobalVariable != "undefined") {
+        result += "myGlobalVariable: " + myGlobalVariable;
+    }
+    if (typeof notGlobalVariable != "undefined") {
+        result += " notGlobalVariable: " + notGlobalVariable;
+    }
+    console.log(result);
+
+}
+function1();
+function2();
+module.exports = {
+    function1,
+    function2
+};
