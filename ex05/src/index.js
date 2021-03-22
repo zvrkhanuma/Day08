07f65992aa323f026375d16d4493d22fc7dab7b6
zvrkhanuma <almazalovic@gmail.com>
2021-03-22 09:30:42 +0100

    Modified exercises

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -1,15 +1,13 @@
 // Only change code below this line
 
 function myDress() {
-    var dress = "ball gown";
-// Only change code above this line
-myDress();
-return dress;
+    dress = "ball gown";
+    // Only change code above this line
+    return dress;
 }
 // Only change code below this line
 var dress = "little dress";
 // Only change code above this line
-console.log(dress);
+console.log(myDress(dress));
 
 module.exports = myDress;
-
--- ex10/src/index.js
+++ ex10/src/index.js
@@ -3,7 +3,7 @@ function mathTest(x, y){
 
 // Only change code above this line
 if  (x < 0  || y < 0 ){
-    // Only change code below this line 
+    // Only change code below this line
     return undefined;
    // Only change code above this line
 
